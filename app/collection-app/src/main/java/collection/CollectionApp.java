a4854affba7c53a8815ddb76279c43f4f4a2d902
NicolasCunha <nicolascunha17@gmail.com>
2023-12-05 18:48:01 -0300

    app structure

--- app/collection-app/src/main/java/collection/CollectionApp.java
+++ app/collection-app/src/main/java/collection/CollectionApp.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 public class CollectionApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Database.bootstrap();
         FXMLLoader fxmlLoader = new FXMLLoader(CollectionApp.class.getResource("collection-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Collection");
